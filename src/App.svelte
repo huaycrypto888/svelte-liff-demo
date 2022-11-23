d05e8db6487f33c5200bd70bac0623273a4356a8
huaycrypto888 <huaycrypto@gmail.com>
2022-11-23 07:01:44 +0700

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -24,7 +24,8 @@
   async function init() {
     let mock = agent.includes("LIFF") ? false : true;
     return await liff.init({
-      liffId: import.meta.env.VITE_LIFF_ID,
+      // liffId: import.meta.env.VITE_LIFF_ID,
+      liffId:"1657611539-z41ew0PW"
       mock,
     });
   }
