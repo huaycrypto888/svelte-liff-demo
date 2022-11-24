12f62de7d55c5711da8d5931ca52d399ec998003
huaycrypto888 <huaycrypto@gmail.com>
2022-11-25 01:06:37 +0700

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -237,7 +237,6 @@
           }
         },
       }]);
-      closeWindow();
   };
 
 
@@ -415,7 +414,8 @@ async function sendMessage() {
 
 <!-- <Geolocation getPosition bind:coords /> -->
 <main>
-  <h1>Liff Demo</h1>
+  <h1>แทงไก่ ออโต้</h1>
+  <h2>การันตี ชื่อนี้ไม่มีเสีย</h2>
   {#await promise}
     <p>LIFF init...</p>
   {:then}
@@ -515,36 +515,6 @@ async function sendMessage() {
   touch-action: manipulation;
 
   cursor: pointer;
-  width: 170px;
+
 }
-
-
-/* .button-88 {
-  display: flex;
-  align-items: center;
-  font-family: inherit;
-  font-weight: 500;
-  font-size: 16px;
-  padding: 0.7em 1.4em 0.7em 1.1em;
-  color: white;
-  background: #ad5389;
-  background: linear-gradient(0deg, rgba(20,167,62,1) 0%, rgba(102,247,113,1) 100%);
-  border: none;
-  box-shadow: 0 0.7em 1.5em -0.5em #14a73e98;
-  letter-spacing: 0.05em;
-  border-radius: 20em;
-  cursor: pointer;
-  user-select: none;
-  -webkit-user-select: none;
-  touch-action: manipulation;
-  width: 200px;
-}
-
-.button-88:hover {
-  box-shadow: 0 0.5em 1.5em -0.5em #14a73e98;
-}
-
-.button-88:active {
-  box-shadow: 0 0.3em 1em -0.5em #14a73e98;
-} */
 </style>
